862dce97b7771be63dba1a14a745969f87680b21
Scott Dougal <scott.dougal@envoyat.com>
2023-03-21 16:51:00 +1100

    added some more functionality

--- src/app/recipes/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipes/recipe-list/recipe-list.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe-list/recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.css']
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent {
-
+  recipes: Recipe[] = [
+    //added from the Recipe model class to model the same recipe style for each recipe that will be created/intansaiated
+    new Recipe(
+      'Test recipe',
+      'description test',
+      'https://www.recipetineats.com/wp-content/uploads/2014/12/One-Pot-mexican-chicken-and-rice_3.jpg'
+    ),
+    new Recipe(
+      'Steak recipe',
+      'it takes so good becasue I made it!',
+      'https://cdn.shopify.com/s/files/1/0586/3630/4429/products/beef-box_c0775a6f-439c-441b-aad4-f92506a6df1e.jpg?v=1655046458'
+    ),
+  ];
 }
